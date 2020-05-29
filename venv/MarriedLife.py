72bdd1f99b8432c116516c791a2938b33698e3f5
AndreiPiterbarg <andrei.piterbarg@yahoo.com>
2020-05-29 18:31:23 +0100

    This is the second commit.

--- venv/MarriedLife.py
+++ venv/MarriedLife.py
@@ -4,7 +4,7 @@ import plotly.express as px
 import seaborn as sns
 
 df = pd.read_csv("Data/mafs.csv")
-print(df.info())
+
 
 # --------------------------- SIMPLE BAR CHART - Region  --------------------------------------
 # grs = df.groupby(["Location"])[["Decision"]].count().reset_index()
@@ -16,23 +16,57 @@ print(df.info())
 #              color='Decision',
 #              barmode='stack'
 #              )
-# --------------------------- SIMPLE BAR CHART(2) - Region  --------------------------------------
-grs = df.groupby(["Age"])[["Decision"]].count().reset_index()
-a = df.loc[df['Decision']=='Yes'].agg(['count'])
-b = df.loc[df['Decision']=='No'].agg(['count'])
+# # --------------------------- SIMPLE BAR CHART(2) - Region  --------------------------------------
+# grs = df.groupby(["Location","Decision"])[["Decision"]].size().reset_index(name='Yes')
+# grs_participants = df.groupby(["Location"])[["Decision"]].size().reset_index(name='Participants_qt')
+#
+# grs_yes = grs[grs['Decision']=='Yes']
+#
+# grs_yes.set_index('Location', inplace=True)
+# grs_participants.set_index('Location', inplace=True)
+#
+# grs_yes["ratio"] = grs_yes["Yes"]/grs_participants["Participants_qt"]
+#
+# grs_yes.reset_index(inplace = True)
+#
+# fig = px.bar(grs_yes,
+#              x='Location',
+#              y= 'ratio',
+#              title='Test',
+#              color='Location',
+#              barmode='stack'
+#              )
+# --------------------------- IMPOSSIBLE BAR CHART(3) - Region  --------------------------------------
+# grs = df.groupby(["Age","Decision"])[["Decision"]].size().reset_index(name='Yes')
+# grs_participants = df.groupby(["Age"])[["Decision"]].size().reset_index(name='Participants_qt')
+#
+# grs_yes = grs[grs['Decision']=='Yes']
+#
+# grs_yes.set_index('Age', inplace=True)
+# grs_participants.set_index('Age', inplace=True)
+#
+# grs_yes["ratio"] = grs_yes["Yes"]/grs_participants["Participants_qt"]
+#
+# grs_yes.reset_index(inplace = True)
+#
+# print(grs_yes)
+#
+# fig = px.bar(grs_yes,
+#              x='Age',
+#              y= 'ratio',
+#              title='Test',
+#              color='Age',
+#              barmode='stack'
+#              )
 
-fig = px.bar(grs,
-             x='Age',
-             y= a/b,
-             title='Test',
-             color='Decision',
-             barmode='stack'
-             )
-# --------------------------- STACKED BAR CHART - Region  --------------------------------------
-# grgs = df.groupby(["Sex","Ethnicity"])[["Attainment8"]].mean().reset_index()
-# fig = px.bar(grgs,
-#              x="Sex",
-#              y="Attainment8",
-#              color='Ethnicity',
-#              barmode='stack')
-fig.show()
+# --------------------------- IMPOSSIBLE BAR CHART(3) - Region  --------------------------------------
+grs = df.groupby(["Decision"])[["Decision"]].size().reset_index(name='Yes')
+grs_married = df.groupby(["Status"])[["Decision"]].size().reset_index(name='StillMarried')
+
+grs_yes = grs[grs['Decision']=='Yes']
+grs_married = grs_married[grs_married['Status']=='Married']
+
+grs_yes["ratio"] = grs_married["StillMarried"]/grs_yes["Yes"]
+
+print(grs_yes)
+
